d39e4611d1697dd3c2f56aaa5d035f36aaa6f850
prashant19-hub <prasha5711nt@gmail.com>
2026-08-27 23:59:12 +0530

    AlgomaniC

--- algomaniac/main.go
+++ algomaniac/main.go
@@ -12,3 +12,10 @@ func main(){
 		fmt.Println("NO")
 	}
 }
+
+// Algomaniac Finals
+// Algomaniac finals, a part of Convolution Fest of Jadavpur University, will be held on March 1717.
+// Shreyan can only go to Jadavpur University on March XX.
+// Print YAY if he can attend the Algomaniac finals and NO if he cannot.
+// Input Format
+// The first and only line of input contains one integer, XX, the day of march Shreyan can go to Jadavpur University. 
